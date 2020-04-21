fbd7c74e4d9316b64f644adbf247bc57f4a2d27b
David Valencia <davidvalenciac9@gmail.com>
2020-04-21 18:47:58 -0400

    adding upvoting and downvoting feature

--- src/Joke.js
+++ src/Joke.js
@@ -2,10 +2,32 @@ import React, {Component} from 'react';
 import './Joke.css';
 
 export class Joke extends Component {
+  constructor (props) {
+    super (props);
+
+    this.upVote = this.upVote.bind (this);
+    this.downVote = this.downVote.bind (this);
+  }
+
+  upVote () {
+    this.props.upVote (this.props.id);
+  }
+
+  downVote () {
+    this.props.downVote (this.props.id);
+  }
+
   render () {
     return (
       <div className="Joke">
-        <p>{this.props.joke}</p>
+        <div className="score">
+          <i onClick={this.upVote} className="fas fa-arrow-up" />
+          {/*Displaying Score*/}
+          <span className="score-display">{this.props.score}</span>
+          <i onClick={this.downVote} className="fas fa-arrow-down" />
+        </div>
+        <div className="joke-text">{this.props.joke}</div>
+
       </div>
     );
   }
--- src/JokesDisplay.js
+++ src/JokesDisplay.js
@@ -1,6 +1,8 @@
 import React, {Component} from 'react';
 import Joke from './Joke';
+import './JokesDisplay.css';
 import axios from 'axios';
+import '@fortawesome/fontawesome-free/css/all.css';
 const API_URL = 'https://icanhazdadjoke.com/search?limit=10&page=1';
 
 export class JokesDisplay extends Component {
@@ -8,21 +10,58 @@ export class JokesDisplay extends Component {
     super (props);
 
     this.state = {jokesList: []};
+    this.upVote = this.upVote.bind (this);
+    this.downVote = this.downVote.bind (this);
   }
+
+  [{id: 1234, joe: 'aca el chiste', score: 0}];
 
   async componentDidMount () {
     let response = await axios.get (API_URL, {
       headers: {Accept: 'application/json'},
     });
+    let results = response.data.results;
+    results.forEach (r => {
+      r.score = 0;
+    });
     this.setState (st => ({
       jokesList: response.data.results,
     }));
-    console.log (response);
+    console.log (results);
   }
+
+  //UpVote and DownVote handlers
+  //Seting State creating a new array with map, looking for a matching id and +/- 1 to current score
+
+  upVote (id) {
+    this.setState (st => ({
+      jokesList: st.jokesList.map (
+        j => (j.id === id ? {...j, score: j.score++} : j)
+      ),
+    }));
+  }
+
+  downVote (id) {
+    this.setState (st => ({
+      jokesList: st.jokesList.map (
+        j => (j.id === id ? {...j, score: j.score--} : j)
+      ),
+    }));
+  }
+  //End of UpVote and DownVote handlers
 
   render () {
     const joke = this.state.jokesList.map (j => {
-      return <Joke joke={j.joke} />;
+      return (
+        <Joke
+          joke={j.joke}
+          upVote={this.upVote}
+          downVote={this.downVote}
+          score={j.score}
+          id={j.id}
+          key={j.id}
+        />
+      );
     });
     return (
       <div className="JokesDisplay">
